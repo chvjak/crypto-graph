473dd875d2805ec3096870c267421047ccc3e774
Dmytro Arkhypenko <Dmytro.Arkhypenko@simcorp.com>
2021-02-22 12:11:37 +0100

    Added a web graph; ading order boog

--- PyGraph1/PyGraph1.py
+++ PyGraph1/PyGraph1.py
@@ -21,8 +21,6 @@ def rescale_ob(ob_prices, ob_volumes, time_span_length, volume_per_time_unit):
 
 plt.show()
 #plt.autoscale()
-#axes = plt.gca()
-#axes.set_ylim((55000, 60000))
 
 ax = plt.axes()
 
@@ -31,30 +29,32 @@ trade_prices = []
 trade_times = []
 trade_volumes = []
 
+ob_deck = []
+MAX_OB_DECK = 50
+
 while True:
 
     plt.clf()
-    plt.grid(b = True, c="w")
+    plt.grid(b = True, c="#006400")
     ret = r.get("https://api-pub.bitfinex.com/v2/trades/tBTCUSD/hist")
     values = ret.json()
     trade_prices += [v[-1] for v in values]
     trade_times += [v[1] for v in values]
     trade_volumes += [abs(v[-2]) for v in values]
-    trade_volumes1 = [v[-2] for v in values]
+
     sort_keys = [i for i in range(len(trade_times))]
     sort_keys.sort(key=lambda i: trade_times[i])
     
     trade_times = [trade_times[i] for i in sort_keys]
     trade_prices = [trade_prices[i] for i in sort_keys]
     trade_volumes = [trade_volumes [i] for i in sort_keys]
-    trade_volumes1 = [trade_volumes1 [i] for i in sort_keys]
 
     max_volume = max(trade_volumes)
     min_volume = min(trade_volumes)
     volume = sum(trade_volumes)                                  # TODO: investigate if the trades are recorded twice - as SELL and as BUY - in this case the VOLUME is doubled
     
     time_span_length = trade_times[-1] - trade_times[0]
-    volume_per_time_unit = volume / (trade_times[-1] - trade_times[0])                               # per second?
+    volume_per_time_unit = volume / (trade_times[-1] - trade_times[0])                               # per millisecond
 
     plt.plot(trade_times, trade_prices, 'b')
     volume_color_values = [100 + int(155 / (max_volume - min_volume) * v) for v in trade_volumes]
@@ -76,11 +76,17 @@ while True:
     scaled_up = rescale_ob(up, up_volume_list, time_span_length, volume_per_time_unit)
     scaled_down = rescale_ob(down, down_volume_list, time_span_length, volume_per_time_unit)
 
-    step_up = 1
-    step_down = 1
+    ob_deck.append((trade_times[-1], scaled_up, scaled_down))
 
-    plt.plot([trade_times[-1] + step_up * i for i in range(len(scaled_up))], scaled_up, 'r')
-    plt.plot([trade_times[-1] + step_down * i for i in range(len(scaled_down))], scaled_down, '#00FF00')
+    if len(ob_deck) > MAX_OB_DECK:
+        ob_deck.pop(0)
+
+    color_step = int(255 / (len(ob_deck)))
+    color = 0
+    for st, su, sd in ob_deck:
+        color += color_step 
+        plt.plot([st + i for i in range(len(su))], su, '#%02x0000' % color)
+        plt.plot([st + i for i in range(len(sd))], sd, '#00%02x00' % color)
 
     plt.pause(0.005) # for better animation see https://matplotlib.org/3.1.1/api/animation_api.html#module-matplotlib.animation
 
