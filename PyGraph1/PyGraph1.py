e8970fb6acbab4ed0e1af4bdd45a6571e8a8239b
Dmytro Arkhypenko <Dmytro.Arkhypenko@simcorp.com>
2021-02-28 14:20:23 +0100

    Added agregation (through loading candles)

--- PyGraph1/PyGraph1.py
+++ PyGraph1/PyGraph1.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import requests as r
 import bisect as bs
 from collections import deque 
+import datetime
 import Utils
+
+AGGREGATION_PERIOD_LENGTH = 60 * 1000 
 
 def rescale_ob(ob_prices, ob_volumes, time_span_length, volume_per_time_unit):
     ps_up_volume = ob_volumes[:]            # prefix sum
@@ -12,8 +15,10 @@ def rescale_ob(ob_prices, ob_volumes, time_span_length, volume_per_time_unit):
     
     expected_volume = 0.0
     scaled_ob_prices = []
-    for t in range(time_span_length):
-        expected_volume += volume_per_time_unit
+    
+    time_step = AGGREGATION_PERIOD_LENGTH 
+    for t in range(0, time_span_length, time_step):
+        expected_volume += volume_per_time_unit * time_step
         ix = bs.bisect_right(ps_up_volume, expected_volume)
         if ix < len(ob_prices):
             scaled_ob_prices.append(ob_prices[ix])                           #price found by expected volume
@@ -22,7 +27,15 @@ def rescale_ob(ob_prices, ob_volumes, time_span_length, volume_per_time_unit):
     return scaled_ob_prices
 
 
+#P0 x6 volumes
+#P1 [45040, 45040, 45020, 45010, 45000, 45000, 45000, 44980, 44980, 44970]
+#P2 [44900, 44900, 44900, 44900, 44900, 44900, 44900, 44900, 44900, 44900]
+#P3 [44000, 44000, 44000, 44000, 44000, 44000, 44000, 44000, 44000, 44000]
+#Level of price aggregation (P0, P1, P2, P3, P4, R0)
+
+
 plt.show()
+plt.style.use('dark_background')        # SEE ALSO: https://stackoverflow.com/questions/23444413/change-a-charts-border-area-color
 
 # TODO: longer time period to plot - 10-20 mins, on web gui - add a control
 # TODO: control  for different OB aggregations
@@ -33,11 +46,11 @@ trade_prices = deque(maxlen = MAX_TRADE_LEN)
 trade_times = deque(maxlen = MAX_TRADE_LEN)
 trade_volumes = deque(maxlen = MAX_TRADE_LEN)
 
-MAX_OB_DECK = 3
+MAX_OB_DECK = 10
 ob_deck = deque(maxlen = MAX_OB_DECK)
 
 def load_ob():
-    ret = r.get("https://api-pub.bitfinex.com/v2/book/tBTCUSD/P0")
+    ret = r.get("https://api-pub.bitfinex.com/v2/book/tBTCUSD/P2")
     values = ret.json()
     up = [v[0] for v in values if v[-1] > 0]                    # price levels
     up_volume_list = [v[-1] for v in values if v[-1] > 0]       # volumes
@@ -51,7 +64,11 @@ while True:
     plt.clf()
     plt.grid(b = True, c="#006464")
 
-    truncated_prices, truncated_volumes, tuncated_times = Utils.load_trades((1 + trade_times[-1]) if len(trade_times) else 0)
+    aggregation_period = AGGREGATION_PERIOD_LENGTH
+    ts_now = datetime.datetime.now().timestamp() * 1000 
+    limit_time  = (1 + trade_times[-1]) if len(trade_times) else (ts_now - aggregation_period * 10)
+
+    truncated_prices, truncated_volumes, tuncated_times = Utils.load_aggregated_trades(limit_time, "1m")
 
     trade_times += tuncated_times
     trade_prices += truncated_prices
@@ -67,13 +84,13 @@ while True:
     min_volume = min(trade_volumes)
 
     volume_color_values = [100 + int(155 / (max_volume - min_volume) * v) for v in trade_volumes]
-    volume_colors = ['#%02x0000' % v for v in volume_color_values]
+    volume_colors = ['#%02x0000' % int(v) for v in volume_color_values]
 
-    plt.scatter(trade_times, trade_prices, c=volume_colors)
+    #plt.scatter(trade_times, trade_prices, c=volume_colors)
+    plt.scatter(trade_times, trade_prices)
 
     # plot the order book
     down, down_volume_list, up, up_volume_list = load_ob()
-
 
     volume = sum(trade_volumes)                                     # SEEMS NOT: if the trades are recorded twice - as SELL and as BUY - in this case the VOLUME is doubled
                                                                     # MAYBE: check if signed_trade_volumes offset over time
@@ -89,9 +106,9 @@ while True:
     color = 0
     for st, su, sd in ob_deck:
         color += color_step 
-        plt.plot([st + i for i in range(len(su))], su, '#%02x0000' % color)
-        plt.plot([st + i for i in range(len(sd))], sd, '#00%02x00' % color)
+        plt.plot([st + AGGREGATION_PERIOD_LENGTH * i for i in range(len(su))], su, '#%02x0000' % color)
+        plt.plot([st + AGGREGATION_PERIOD_LENGTH * i for i in range(len(sd))], sd, '#00%02x00' % color)
 
-    plt.pause(0.005) # for better animation see https://matplotlib.org/3.1.1/api/animation_api.html#module-matplotlib.animation
-
+    #plt.pause(0.005) # for better animation see https://matplotlib.org/3.1.1/api/animation_api.html#module-matplotlib.animation
+    plt.pause(30) # should correspond to aggregation
 
--- PyGraph1/Utils.py
+++ PyGraph1/Utils.py
@@ -1,10 +1,46 @@
 import requests as r
+from collections import namedtuple
+CandlePrices = namedtuple("CandlePrices", ["open", "close", "high", "low"])
+
+def load_candles(limit_time, aggregation="1m"):
+    '''
+    aggregation = 1m, 5m, ...
+    '''
+
+    url = "https://api-pub.bitfinex.com/v2/candles/trade:{1}:tBTCUSD/hist?sort=1&start={0}".format(limit_time, aggregation)
+
+    ret = r.get(url)
+    values = ret.json()
+
+    #[1614459540000,47076,47080,47091,47052,1.51903903]
+    #[ts, open, close, high, low, vol]
+    candle_times = [v[0] for v in values]
+    candle_prices = [CandlePrices(*v[1:5]) for v in values]
+    candle_volumes = [abs(v[5]) for v in values]
+
+    return candle_prices, candle_volumes, candle_times
+
+def candles2trades(candle_prices, candle_volumes, candle_times):
+    trade_times = []
+    trade_prices = []
+    trade_volumes = []
+
+    if len(candle_prices) < 2:
+        return [], [], []
+
+    time_span = candle_times[1] - candle_times[0]
+    for candle_4price, candle_volume, candle_time in zip(candle_prices, candle_volumes, candle_times):
+        trade_times += [candle_time + time_span // 4 * t for t in range(4)]
+        trade_prices += [candle_4price.open, candle_4price.low, candle_4price.high, candle_4price.close]
+        trade_volumes += [candle_volume / 4] * 4
+
+    return trade_prices, trade_volumes, trade_times
+
+def load_aggregated_trades(limit_time, aggregation):
+    return candles2trades(*load_candles(limit_time, aggregation))
 
 def load_trades(limit_time):
-    if limit_time:
-        url = "https://api-pub.bitfinex.com/v2/trades/tBTCUSD/hist?sort=1&start={}&".format(limit_time)
-    else:
-        url = "https://api-pub.bitfinex.com/v2/trades/tBTCUSD/hist"
+    url = "https://api-pub.bitfinex.com/v2/trades/tBTCUSD/hist?sort=1&start={}".format(limit_time)
 
     ret = r.get(url)
     values = ret.json()
